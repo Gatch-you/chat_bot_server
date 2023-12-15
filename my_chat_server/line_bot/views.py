9dba99b2c116e3da42226fcbd5f9e052ec9ab664
Gatch-you <yutohagaeng@gmail.com>
2023-12-15 19:48:45 +0900

    create line response

--- my_chat_server/line_bot/views.py
+++ my_chat_server/line_bot/views.py
@@ -1,8 +1,13 @@
+import json
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .chat_with_gpt.conversation import create_single_text_message
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .chat_with_gpt.conversation import chat_with_bot
+from .chat_with_gpt.line_message import LineMessage
 
 # Create your views here.
 class ChatwitBotView(APIView):
@@ -12,3 +17,18 @@ class ChatwitBotView(APIView):
     def post(self, request):
         bot_response = chat_with_bot(request.data["message"])
         return Response(bot_response, status=status.HTTP_200_OK)
+
+
+@csrf_exempt
+def index(request):
+    print("thread")
+    if request.method == 'POST':
+        request = json.loads(request.body.decode('utf-8'))
+        print(request)
+        data = request['events'][0]
+        message = request['events'][0]['message']
+        user_id = request['events'][0]['source']['userId']
+        reply_token = data['replyToken']
+        line_message = LineMessage(create_single_text_message(user_id, message['text']))
+        line_message.reply(reply_token)
+        return HttpResponse("ok")
